35cd777b7c2349993ee4cfa1b08c7b434efbe34b
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-01-10 01:06:52 +0000

    Redesigned the window to be custom shaped by discarding the default frame decoration.

--- privacy_provider/src/main/java/com/raman/file_encryption/gui/Toast.java
+++ privacy_provider/src/main/java/com/raman/file_encryption/gui/Toast.java
@@ -1,111 +1,72 @@
 package com.raman.file_encryption.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
+import java.awt.Rectangle;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.RoundRectangle2D;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
 
-
-public class Toast extends JFrame
+class Toast extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 	private JLabel bodyMessage;
-	
+	private final int frameWidth = 300;
+	private final int frameHeight = 280;
 	public Toast()
 	{
-		setUp_Frame();
-		set_ui_components();
+		setUIComponents();
+		setup_frame();
 	}
 	
-	private void setUp_Frame()
+	private void setup_frame()
 	{
-		setPreferredSize(new Dimension(400, 400));
-	    setResizable(false);
+		setPreferredSize(new Dimension(frameWidth, frameHeight));
+		setTitle("Toast");
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout(FlowLayout.CENTER));
+		getContentPane().setBackground(Color_Converter.hex_to_rgb("#e8f9cf"));
+		add(new JLabel("Boo!"));
+		setUndecorated(true);
+		//Rounded shape
+		//setShape(new Ellipse2D.Double(0, 0, 200, 200));
+		setShape(only_two_rounded_corners());
 		pack();
 		setLocationRelativeTo(null);
+		//The window must be undecorated first.
 		//setOpacity(0);
-		//setLayout(new GridBagLayout());
-		//setBackground(Color_Converter.hex_to_rgb("#e8f9cf"));
-
-		/*
-		addComponentListener(new ComponentListener() {
-			
-			@Override
-			public void componentShown(ComponentEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void componentResized(ComponentEvent e) 
-			{
-				setShape(new  RoundRectangle2D.Double(0,0,getWidth(),
-			    getHeight(), 20, 20));  
-			}
-			
-			@Override
-			public void componentMoved(ComponentEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void componentHidden(ComponentEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		*/
 	}
 	
-	private void set_ui_components()
+	private Area only_two_rounded_corners()
+	{
+		int width = getPreferredSize().width;
+		int height = getPreferredSize().height;
+		Area shape1 = new Area(new RoundRectangle2D.Double(0, 0, width, height, 50, 50));
+        Area shape2 = new Area(new Rectangle(0, height - 50, width, height / 2));
+        shape1.add(shape2);
+        return shape1;
+	}
+
+	private void setUIComponents()
 	{
 		bodyMessage = new JLabel();
 		add(bodyMessage);
 	}
 	
-	public void setBodyText(String message)
-	{
-		bodyMessage.setText(message);
-	}
 	
-	public void show()
+	public void setBodyMessage(String msg)
 	{
-		setVisible(true);
-		/*
-		try {
-	         
-		     //hide the toast message in slow motion
-	         for (double d = 0.2; d < 1.0; d += 0.1) {
-	            Thread.sleep(100);
-	            setOpacity((float)d);
-	         }
-	      }catch (Exception e) {
-	         System.out.println(e.getMessage());
-	      }
-	      
-	     */ 
-	}
-	
-	public void close()
-	{
-		try {
-		     //hide the toast message in slow motion
-	         for (double d = 1.0; d > 0.2; d -= 0.1) {
-	            Thread.sleep(100);
-	            setOpacity((float)d);
-	         }
-	         // set the visibility to false
-	         setVisible(false);
-	      }catch (Exception e) {
-	         System.out.println(e.getMessage());
-	      }
+		bodyMessage.setText(msg);
 	}
 	
 	
@@ -113,9 +74,35 @@ public class Toast extends JFrame
 	{
 		//Run the GUI code on the event-dispatching thread
 		SwingUtilities.invokeLater(()->{
-			Toast toast = new Toast();
-			toast.setBodyText("Hi...");
-			toast.setVisible(true);
+			Toast t = new Toast();
+			//toast.setBodyText("Hi...");
+			t.setBodyMessage("Raman");
+			t.setVisible(true);
 		});
 	}
+
 }
+
+
+/**
+	
+	public void show()
+	{
+		setVisible(true);
+		try {
+			
+		     //hide the toast message in slow motion
+	         for (double d = 0.2; d < 1.0; d += 0.1) {
+	            //Thread.sleep(100);
+	            setOpacity((float)d);
+	         }
+	      }catch (Exception e) {
+	         System.out.println(e.getMessage());
+	      }
+	}
+	
+ 
+ 
+ 
+ 
+ */
