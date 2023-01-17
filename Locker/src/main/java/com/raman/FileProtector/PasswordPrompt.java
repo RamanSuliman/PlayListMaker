2c372e1bf99b38a62849e3226d87de682471603a
Raman Suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-01-17 14:37:57 +0000

    Delete Locker directory

--- Locker/src/main/java/com/raman/FileProtector/PasswordPrompt.java
+++
@@ -1,6 +0,0 @@
-package com.raman.FileProtector;
-
-public class PasswordPrompt 
-{
-	
-}
--- Locker/src/main/java/com/raman/main/TaskChooser.java
+++
@@ -1,8 +0,0 @@
-package com.raman.main;
-
-import javafx.stage.Stage;
-
-public class TaskChooser extends Stage
-{
-	
-}
